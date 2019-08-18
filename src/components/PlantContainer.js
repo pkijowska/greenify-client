57570eb431b80467fd85b5e7bc9a36b94c7364de
Paulina <p.kijowska@gmail.com>
2019-08-18 15:54:16 +1000

    minor filter changes

--- src/components/PlantContainer.js
+++ src/components/PlantContainer.js
@@ -6,71 +6,82 @@ import axios from 'axios';
 const SERVER_URL = 'http://localhost:3000/plants.json'
 
 class PlantContainer extends Component {
-constructor() {
-  super();
-  this.state={
-    plants : [],
-    query : ""
-  }
+  constructor() {
+    super();
+    this.state={
+      plants : [],
+      query : ""
+    };
+
   this._handleSubmit = this._handleSubmit.bind(this);
   this._handleInputSearch = this._handleInputSearch.bind(this);
-
-
 
 
 const fetchPlants =() => {
   axios.get(SERVER_URL).then((result) => {
     this.setState({plants: result.data});
-
+  console.log(this.state.plants);
     setTimeout(fetchPlants, 40000);
-  });
-};
-fetchPlants();
-}
+            });
+          };
+          fetchPlants();
+          }
 
-_handleSubmit (event) {
-  event.preventDefault();
-}
+    _handleSubmit (event) {
+      event.preventDefault();
+      this.setState({query: '' })
+      console.log("click");
+          }
 
-// change the state value to equal the string input.
-_handleInputSearch (event) {
 
-  this.setState({
-        query: event.target.value,
-      })
-}
+   _handleInputSearch (event) {
+    this.setState({
+    query: event.target.value,})
+    console.log(event.target.value);
+          }
 
 
 
   render(){
     return(
       <div>
-      <button><Link to="/">Go back!</Link></button>
-      <h2>This is a plant area. See our plants:</h2>
-      <Gallery plants={this.state.plants}/>
+        <button><Link to="/">Go back!</Link></button>
+
+
+
+        <h2>This is a plant area. See our plants:</h2>
+        <form onSubmit={this._handleSubmit}>
+
+        <label htmlFor="query">
+        <p>Search:</p>
+        <input type="search" id="query" onInput={ this._handleInputSearch } />
+        </label>
+        <input type="submit" value="Filter" />
+        </form>
+
+        <Gallery plants={this.state.plants}/>
       </div>
     )
   }
 }
+
 
 
 class Gallery extends Component {
   render() {
     return(
-      <div>
-{this.props.plants.map((plant) => <p key={plant.id}> {plant.name}
-   <Link to={ "/plants/" + plant.id }>
-   <Image cloudName="dto4pzoz6" publicId={plant.images} width="300" />
-   </Link>
-</p>)
-}
-      </div>
 
+      <div>
+        {this.props.plants.map((plant) => <p key={plant.id}> {plant.name}
+           <Link to={ "/plants/" + plant.id }>
+           <Image cloudName="dto4pzoz6" publicId={plant.images} width="300" />
+           </Link>
+        </p>)
+            }
+    </div>
     )
   }
 }
-
-
 
 
 export default PlantContainer;
