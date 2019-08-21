eda4f17ea5d7d07b396c20c535e84a08efaca3eb
Paulina <p.kijowska@gmail.com>
2019-08-21 10:29:21 +1000

    Updates

---
+++ precache-manifest.ca793dfe1fbd41b8d6b64d411cd53f64.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70cb31569c914c8a87689502346eb0b",
+    "url": "/greenify-client/index.html"
+  },
+  {
+    "revision": "9f8422e1eba7782ec798",
+    "url": "/greenify-client/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "2b374a6d942af59d086a",
+    "url": "/greenify-client/static/css/main.d101057c.chunk.css"
+  },
+  {
+    "revision": "9f8422e1eba7782ec798",
+    "url": "/greenify-client/static/js/2.e665fab3.chunk.js"
+  },
+  {
+    "revision": "2b374a6d942af59d086a",
+    "url": "/greenify-client/static/js/main.10fa6e56.chunk.js"
+  },
+  {
+    "revision": "2ad510f999239f32cdeb",
+    "url": "/greenify-client/static/js/runtime~main.a51f914c.js"
+  },
+  {
+    "revision": "8547ceae0d543a17c0ca6f55ff13e606",
+    "url": "/greenify-client/static/media/boy-1.8547ceae.svg"
+  },
+  {
+    "revision": "eae74d360ef1ff3598ae91b8d16c8b28",
+    "url": "/greenify-client/static/media/boy.eae74d36.svg"
+  },
+  {
+    "revision": "d8f7b140b9748c5ce352b12a08693b5e",
+    "url": "/greenify-client/static/media/dirt_short.d8f7b140.png"
+  },
+  {
+    "revision": "038dbd32d31ac61f875c7a8abf453e6c",
+    "url": "/greenify-client/static/media/girl-1.038dbd32.svg"
+  },
+  {
+    "revision": "cd63945d4dd703b4d0670126009a2a1d",
+    "url": "/greenify-client/static/media/girl.cd63945d.svg"
+  },
+  {
+    "revision": "173f02d7a537022054097ed18dd4544f",
+    "url": "/greenify-client/static/media/leaf_dark.173f02d7.png"
+  },
+  {
+    "revision": "0ed838a16f50d59aabcfd9f468e359f7",
+    "url": "/greenify-client/static/media/leaf_green.0ed838a1.png"
+  },
+  {
+    "revision": "dabcdaa294004a1e59704d4837497446",
+    "url": "/greenify-client/static/media/man-1.dabcdaa2.svg"
+  },
+  {
+    "revision": "333fa0cb9582b80cdb755d2fc3f302d1",
+    "url": "/greenify-client/static/media/man-2.333fa0cb.svg"
+  },
+  {
+    "revision": "fcfc134f176b4964023a2c9895a3323b",
+    "url": "/greenify-client/static/media/man-3.fcfc134f.svg"
+  },
+  {
+    "revision": "bef3aabba1d2bf71eded84320d9d7df5",
+    "url": "/greenify-client/static/media/man-4.bef3aabb.svg"
+  },
+  {
+    "revision": "93ef09bc7c0695fa9171505df5edc68b",
+    "url": "/greenify-client/static/media/man.93ef09bc.svg"
+  },
+  {
+    "revision": "841213cf35021fc133b444feb698c9dd",
+    "url": "/greenify-client/static/media/plant_feature.841213cf.png"
+  }
+]);
