309a1b01f3946f242f08c5c945863f517e52ded8
Gopi Patel <ask2gopi@gmail.com>
2019-08-23 11:48:42 +1000

    quiz changes

--- src/ServerURL.js
+++ src/ServerURL.js
@@ -1,6 +1,6 @@
 const serverURL = function (path) {
-  return "http://localhost:3001/" + path;
-  // return "https://greenify-your-life.herokuapp.com/" + path;
+  // return "http://localhost:3000/" + path;
+  return "https://greenify-your-life.herokuapp.com/" + path;
 };
 
 export default serverURL;
